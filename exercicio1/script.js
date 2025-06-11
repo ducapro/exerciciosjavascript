a01c4458622fa40e50f10305cc768f2341640edd
Eduardo Siqueira <eduardo.siqueira@ifb.edu.br>
2025-06-11 10:36:55 -0300

    adiciona funcionalidade da soma

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -2,3 +2,14 @@
 as operações de soma, subtração, multiplicação e divisão. 
 Utilize Javascript para capturar os valores digitados e exibir o 
 resultado da operação selecionada.*/
+var valor1 = document.querySelector("#valor1");
+var valor2 = document.querySelector("#valor2");
+var soma = document.querySelector("#soma");
+var subtracao = document.querySelector("#subtracao");
+var multiplicacao = document.querySelector("#multiplicacao");
+var divisao = document.querySelector("#divisao");
+
+soma.addEventListener("click", () => {
+    resultado = parseFloat(valor1.value) + parseFloat(valor2.value);
+    alert(resultado);
+});
