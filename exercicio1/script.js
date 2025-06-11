e46aa9ffdc00f5fb57897f41946f1dec178a7995
Eduardo Siqueira <eduardo.siqueira@ifb.edu.br>
2025-06-11 10:21:08 -0300

    cria dois campos de entrada

---
+++ exercicio1/script.js
@@ -0,0 +1,4 @@
+/* Crie uma página HTML com dois campos de entrada de números e botões para 
+as operações de soma, subtração, multiplicação e divisão. 
+Utilize Javascript para capturar os valores digitados e exibir o 
+resultado da operação selecionada.*/
